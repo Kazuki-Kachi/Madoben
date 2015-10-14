b24d9b11fff1e9948223fc3b2f1cb1c1eb8309b7
Kazuki Kachi <ale-531_blk@hotmail.co.jp>
2015-10-15 01:14:25 +0900

    Create

---
+++ SushiGoRound/SushiGoRound/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+
+namespace SushiGoRound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
