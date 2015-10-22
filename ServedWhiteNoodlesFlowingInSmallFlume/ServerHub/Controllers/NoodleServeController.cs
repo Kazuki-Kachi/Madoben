3625bd1c5c5af56e90b82329963ae781938e9516
Kazuki Kachi <ale-531_blk@hotmail.co.jp>
2015-10-22 18:07:54 +0900

    Edit

---
+++ ServedWhiteNoodlesFlowingInSmallFlume/ServerHub/Controllers/NoodleServeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using ServedWhiteNoodlesFlowingInSmallFlumeLibraries;
+using ServerHub.Hubs;
+
+namespace ServerHub.Controllers
+{
+    public class NoodleServeController : HubController<PushHub>
+    {   
+        public async Task<IHttpActionResult> PostAAA(IReadOnlyList<INoodle> noodles)
+        {
+
+            HubContext.Clients.All.B();
+            return Ok();
+        }
+    }
+}
---
+++ ServedWhiteNoodlesFlowingInSmallFlume/ServerHub/Hubs/PushHub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using ServedWhiteNoodlesFlowingInSmallFlumeLibraries;
+
+namespace ServerHub.Hubs
+{
+    public class PushHub : Hub { }
+}
